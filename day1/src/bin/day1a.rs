cce4ae5573acb586312f7f6c24f8c8f8602b9fe4
André Restivo <andre.restivo@gmail.com>
2022-12-03 19:50:09 +0000

    chore: added day 1 tests

--- day1/src/bin/day1a.rs
+++ day1/src/bin/day1a.rs
@@ -1,11 +1,33 @@
 use global;
 
-pub fn main() {
-  let max = global::read_strings()
+fn main() {
+  let max = calculate_max(&global::read_strings());
+
+  println!("{}", max);
+}
+
+
+fn calculate_max(lines: &Vec<String>) -> i32 {
+  lines
     .split(|l| l == "")
     .map(|e| e.iter().map(|s| s.parse::<i32>().unwrap()))
     .map(|v| v.sum::<i32>())
-    .max();
+    .max().unwrap()
+}
 
-  println!("{}", max.unwrap())
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn calculate_max_works() {
+    let lines: Vec<String> = [
+      "1000", "2000", "3000", "", 
+      "4000", "", 
+      "5000", "6000", "", 
+      "7000", "8000", "9000", "", 
+      "10000"]
+      .iter().map(|s| String::from(*s)).collect();
+    assert_eq!(24000, calculate_max(&lines));
+  }
 }
--- day1/src/bin/day1b.rs
+++ day1/src/bin/day1b.rs
@@ -1,7 +1,12 @@
 use global;
 
 fn main() {
-  let mut elfs: Vec<i32> = global::read_strings()
+  let max = calculate_max(&global::read_strings());
+  println!("{}", max);
+}
+
+fn calculate_max(lines: &Vec<String>) -> i32 {
+  let mut elfs: Vec<i32> = lines
     .split(|l| l == "")
     .map(|e| e.iter().map(|s| s.parse::<i32>().unwrap()))
     .map(|v| v.sum::<i32>())
@@ -9,5 +14,22 @@ fn main() {
 
   elfs.sort();
 
-  println!("{}", elfs.iter().rev().take(3).sum::<i32>())
+  elfs.iter().rev().take(3).sum::<i32>()
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn calculate_max_works() {
+    let lines: Vec<String> = [
+      "1000", "2000", "3000", "", 
+      "4000", "", 
+      "5000", "6000", "", 
+      "7000", "8000", "9000", "", 
+      "10000"]
+      .iter().map(|s| String::from(*s)).collect();
+    assert_eq!(45000, calculate_max(&lines));
+  }
 }
