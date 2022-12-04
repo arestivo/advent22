7f62ca976b557cb650104cde8dd58bf9b7f1825a
André Restivo <andre.restivo@gmail.com>
2022-12-04 11:26:34 +0000

    chore: renamed day4 filters

--- day4/src/bin/day4a.rs
+++ day4/src/bin/day4a.rs
@@ -2,12 +2,12 @@ use day4::{Assignment, filter_assignements};
 
 fn main() {
   let lines = global::read_strings();
-  let total = filter_assignements(lines, is_contained).len();
+  let total = filter_assignements(lines, contains).len();
 
   println!("{total}")
 }
 
-fn is_contained(p: &Assignment) -> bool {
+fn contains(p: &Assignment) -> bool {
   p.0.0 <= p.1.0 && p.0.1 >= p.1.1 || p.1.0 <= p.0.0 && p.1.1 >= p.0.1
 }
 
@@ -16,10 +16,10 @@ mod tests {
   use super::*;
 
   #[test]
-  fn is_contained_works() {
-    assert!(is_contained(&((0, 1),(0, 1))));
-    assert!(is_contained(&((1, 2),(0, 3))));
-    assert!(is_contained(&((0, 3),(1, 2))));
-    assert!(!is_contained(&((1, 2),(2, 3))));
+  fn contains_works() {
+    assert!(contains(&((0, 1),(0, 1))));
+    assert!(contains(&((1, 2),(0, 3))));
+    assert!(contains(&((0, 3),(1, 2))));
+    assert!(!contains(&((1, 2),(2, 3))));
   }
 }
--- day4/src/bin/day4b.rs
+++ day4/src/bin/day4b.rs
@@ -2,12 +2,12 @@ use day4::{Assignment, filter_assignements};
 
 fn main() {
   let lines = global::read_strings();
-  let total = filter_assignements(lines, is_overlapped).len();
+  let total = filter_assignements(lines, overlaps).len();
 
   println!("{total}")
 }
 
-fn is_overlapped(p: &Assignment) -> bool {
+fn overlaps(p: &Assignment) -> bool {
   p.0.0 <= p.1.1 && p.1.0 <= p.0.1
 }
 
@@ -16,11 +16,11 @@ mod tests {
   use super::*;
 
   #[test]
-  fn is_overlapped_works() {
-    assert!(is_overlapped(&((0, 1),(0, 1))));
-    assert!(is_overlapped(&((1, 2),(0, 3))));
-    assert!(is_overlapped(&((0, 3),(1, 2))));
-    assert!(is_overlapped(&((1, 2),(2, 3))));
-    assert!(!is_overlapped(&((1, 2),(3, 4))));
+  fn overlaps_works() {
+    assert!(overlaps(&((0, 1),(0, 1))));
+    assert!(overlaps(&((1, 2),(0, 3))));
+    assert!(overlaps(&((0, 3),(1, 2))));
+    assert!(overlaps(&((1, 2),(2, 3))));
+    assert!(!overlaps(&((1, 2),(3, 4))));
   }
 }
