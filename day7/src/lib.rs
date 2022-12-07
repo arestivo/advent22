df17a07e695037e0a68463932b8bba20dfef37e2
André Restivo <andre.restivo@gmail.com>
2022-12-07 20:34:44 +0000

    chore: simplified day 7

--- day7/src/lib.rs
+++ day7/src/lib.rs
@@ -5,10 +5,9 @@ use regex::Regex;
 
 pub fn read_folders(lines: &Vec<String>) -> HashMap<String, u64> {
   let mut folders = HashMap::new();
-  let mut current = PathBuf::new();
+  let mut current = "".to_string();
 
-  current.push("/");
-  folders.insert(current.as_path().display().to_string(), 0 as u64);
+  folders.insert("".to_string(), 0 as u64);
 
   let cd_folder = Regex::new(r"^\$ cd ([a-z]+)$").unwrap();
   let cd_back = Regex::new(r"^\$ cd \.\.$").unwrap();
@@ -17,19 +16,20 @@ pub fn read_folders(lines: &Vec<String>) -> HashMap<String, u64> {
   for line in lines {
     if cd_folder.is_match(&line) {
       let name = cd_folder.captures_iter(line).next().unwrap().get(1).unwrap().as_str();
-      current.push(name);
-      folders.insert(current.as_path().display().to_string(), 0);
+
+      current = format!("{}/{}", &current, name);
+
+      folders.insert(current.clone(), 0);
     }
 
     if cd_back.is_match(line) {
-      current = current.parent().unwrap().to_path_buf();
+      current = PathBuf::from(current).parent().unwrap().display().to_string();
     }
 
     if file_entry.is_match(line) {
       let result = file_entry.captures_iter(line).next().unwrap();
       let size = result.get(1).unwrap().as_str().parse::<u64>().unwrap();
-      let name = current.as_path().display().to_string();
-      folders.iter_mut().for_each(|p| if name.starts_with(p.0) { *p.1 = *p.1 + size });
+      folders.iter_mut().for_each(|p| if current.starts_with(p.0) { *p.1 = *p.1 + size });
     }
   }
 
